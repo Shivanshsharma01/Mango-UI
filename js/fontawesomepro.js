eb215b207eb825a6c90413a96ac8aba05e689f6d
Shivanshsharma01 <ss33728@gmail.com>
2022-02-18 00:45:28 +0530

    restructure: formatting
    adding variables

--- js/fontawesomepro.js
+++ js/fontawesomepro.js
@@ -16,36 +16,36 @@ window.FontAwesomeKitConfig = {
     },
     "version": "latest"
 };
-! function() {
-    ! function() {
+! function () {
+    ! function () {
         if (!(void 0 === window.Element || "classList" in document.documentElement)) {
             var e, t, n, i = Array.prototype,
                 o = i.push,
                 a = i.splice,
                 s = i.join;
             r.prototype = {
-                add: function(e) {
+                add: function (e) {
                     this.contains(e) || (o.call(this, e), this.el.className = this.toString())
                 },
-                contains: function(e) {
+                contains: function (e) {
                     return -1 != this.el.className.indexOf(e)
                 },
-                item: function(e) {
+                item: function (e) {
                     return this[e] || null
                 },
-                remove: function(e) {
+                remove: function (e) {
                     if (this.contains(e)) {
                         for (var t = 0; t < this.length && this[t] != e; t++);
                         a.call(this, t, 1), this.el.className = this.toString()
                     }
                 },
-                toString: function() {
+                toString: function () {
                     return s.call(this, " ")
                 },
-                toggle: function(e) {
+                toggle: function (e) {
                     return this.contains(e) ? this.remove(e) : this.add(e), this.contains(e)
                 }
-            }, window.DOMTokenList = r, e = Element.prototype, t = "classList", n = function() {
+            }, window.DOMTokenList = r, e = Element.prototype, t = "classList", n = function () {
                 return new r(this)
             }, Object.defineProperty ? Object.defineProperty(e, t, {
                 get: n
@@ -59,16 +59,16 @@ window.FontAwesomeKitConfig = {
 
     function f(e) {
         var t, n, i, o;
-        prefixesArray = e || ["fa"], prefixesSelectorString = "." + Array.prototype.join.call(e, ",."), t = document.querySelectorAll(prefixesSelectorString), Array.prototype.forEach.call(t, function(e) {
+        prefixesArray = e || ["fa"], prefixesSelectorString = "." + Array.prototype.join.call(e, ",."), t = document.querySelectorAll(prefixesSelectorString), Array.prototype.forEach.call(t, function (e) {
             n = e.getAttribute("title"), e.setAttribute("aria-hidden", "true"), i = !e.nextElementSibling || !e.nextElementSibling.classList.contains("sr-only"), n && i && ((o = document.createElement("span")).innerHTML = n, o.classList.add("sr-only"), e.parentNode.insertBefore(o, e.nextSibling))
         })
     }
-    var e, t, u = function(e) {
+    var e, t, u = function (e) {
             var t = document.createElement("link");
             t.href = e, t.media = "all", t.rel = "stylesheet", document.getElementsByTagName("head")[0].appendChild(t)
         },
-        m = function(e) {
-            ! function(e, t, n) {
+        m = function (e) {
+            ! function (e, t, n) {
                 var i, o = window.document,
                     a = o.createElement("link");
                 if (t) i = t;
@@ -80,16 +80,16 @@ window.FontAwesomeKitConfig = {
                 a.rel = "stylesheet", a.href = e, a.media = "only x",
                     function e(t) {
                         if (o.body) return t();
-                        setTimeout(function() {
+                        setTimeout(function () {
                             e(t)
                         })
-                    }(function() {
+                    }(function () {
                         i.parentNode.insertBefore(a, t ? i : i.nextSibling)
                     });
-                var l = function(e) {
+                var l = function (e) {
                     for (var t = a.href, n = r.length; n--;)
                         if (r[n].href === t) return e();
-                    setTimeout(function() {
+                    setTimeout(function () {
                         l(e)
                     })
                 };
@@ -100,7 +100,7 @@ window.FontAwesomeKitConfig = {
                 a.addEventListener && a.addEventListener("load", c), (a.onloadcssdefined = l)(c)
             }(e)
         },
-        n = function(e, t) {
+        n = function (e, t) {
             var n = t && void 0 !== t.autoFetchSvg ? t.autoFetchSvg : void 0,
                 i = t && void 0 !== t.async ? t.async : void 0,
                 o = t && void 0 !== t.autoA11y ? t.autoA11y : void 0,
@@ -125,9 +125,9 @@ window.FontAwesomeKitConfig = {
                 minify: e.minify.enabled
             })), n(h(e, {
                 minify: e.minify.enabled
-            }), t)), "css" === i.method && function(e) {
+            }), t)), "css" === i.method && function (e) {
                 var t, n, i, o, a, s, r, l, c = f.bind(f, ["fa", "fab", "fas", "far", "fal"]);
-                e.autoA11y.enabled && (n = c, o = [], a = document, s = a.documentElement.doScroll, r = "DOMContentLoaded", (l = (s ? /^loaded|^c/ : /^loaded|^i|^c/).test(a.readyState)) || a.addEventListener(r, i = function() {
+                e.autoA11y.enabled && (n = c, o = [], a = document, s = a.documentElement.doScroll, r = "DOMContentLoaded", (l = (s ? /^loaded|^c/ : /^loaded|^i|^c/).test(a.readyState)) || a.addEventListener(r, i = function () {
                     for (a.removeEventListener(r, i), l = 1; i = o.shift();) i()
                 }), l ? setTimeout(n, 0) : o.push(n), t = c, "undefined" != typeof MutationObserver && new MutationObserver(t).observe(document, {
                     childList: !0,
